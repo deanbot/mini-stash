0823110e1c44c6d8798cb1aeb23221c0c2c7497a
Dean Verleger <deanverleger@gmail.com>
2020-07-15 15:44:04 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+// constants
+const storageKeyDateSuffix = '_date';
+
+// helpers 
+export interface IStorage {
+	getItem(key:string):any;
+	setItem(key:string, data:any):void;
+}
+const getStorage = (useSessionStorage?: boolean):IStorage => !useSessionStorage ? localStorage : sessionStorage;
+const getDateKey = (storageKey: string):string => `${storageKey}${storageKeyDateSuffix}`;
+
+// exported methods
+export const stash = (
+	storageKey:string, 
+	data: any,
+	useSessionStorage?: boolean
+) => {
+	const storage = getStorage(useSessionStorage);
+	const time = Math.floor(new Date().getTime() / 1000);
+	storage.setItem(storageKey, JSON.stringify(data));
+	storage.setItem(getDateKey(storageKey), time.toString());
+};
+
+export const retrieve = (
+	storageKey: string,
+	minutesToExpiration: number,
+	useSessionStorage?: boolean
+) => {
+	const storage = getStorage(useSessionStorage);
+	const storageDate = +storage.getItem(getDateKey(storageKey));
+	let data = storage.getItem(storageKey);
+	if (!!data && !!storageDate) {
+		try {
+			const nowMs = +(new Date());
+			const storageMs = +(new Date(storageDate)) * 1000;
+			const minutesOld = Math.ceil(
+				nowMs - storageMs / 1000 / 60
+			);
+			if (minutesOld > minutesToExpiration) {
+				clear(storageKey, useSessionStorage);
+			} else {
+				data = JSON.parse(data);
+			}
+		} catch (e) {}
+	}
+	return data;
+};
+
+export const clear = (
+	storageKey: string,
+	useSessionStorage?: boolean
+) => {
+	const storage = getStorage(useSessionStorage);
+	delete storage[storageKey];
+	delete storage[getDateKey(storageKey)];
+}
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const webpack = require('webpack');
+const dev = process.env.NODE_ENV !== 'production';
+
+const libraryName = 'stash';
+
+const sharedConfig = {
+  resolve: {
+    extensions: ['*', '.ts', '.tsx', '.js', '.json'],
+  },
+  entry: path.join(__dirname, 'src/index.ts'),
+  devtool: 'source-map',
+  mode: dev ? 'development' : 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'babel-loader',
+      }
+    ]
+  }
+};
+
+module.exports = {
+  ...sharedConfig,
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: 'index.js',
+    library: libraryName,
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    umdNamedDefine: true
+  },
+  plugins: dev
+    ? [
+      new HtmlWebpackPlugin({
+        template: 'index.ejs',
+        inject: 'head',
+        templateParameters: {
+          'apiKey': process.env.API_KEY
+        }
+      })
+    ]
+    : []
+};
