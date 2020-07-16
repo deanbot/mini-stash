76de9b127bd80cd01e27d9cc6f1a4d7817581136
Dean Verleger <deanverleger@gmail.com>
2020-07-16 17:33:37 -0500

    fix bug with clear being after retrieve. invalidate data if date is missing

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,59 @@
 // constants
 const storageKeyDateSuffix = '_date';
 
-// helpers 
+// helpers
 export interface IStorage {
-	getItem(key:string):any;
-	setItem(key:string, data:any):void;
+  getItem(key:string):any;
+  setItem(key:string, data:any):void;
 }
 const getStorage = (useSessionStorage?: boolean):IStorage => !useSessionStorage ? localStorage : sessionStorage;
 const getDateKey = (storageKey: string):string => `${storageKey}${storageKeyDateSuffix}`;
 
 // exported methods
-export const stash = (
-	storageKey:string, 
-	data: any,
-	useSessionStorage?: boolean
+export const clear = (
+  storageKey: string,
+  useSessionStorage?: boolean
 ) => {
-	const storage = getStorage(useSessionStorage);
-	const time = Math.floor(new Date().getTime() / 1000);
-	storage.setItem(storageKey, JSON.stringify(data));
-	storage.setItem(getDateKey(storageKey), time.toString());
+  const storage = getStorage(useSessionStorage);
+  delete storage[storageKey];
+  delete storage[getDateKey(storageKey)];
+};
+
+export const stash = (
+  storageKey:string,
+  data: any,
+  useSessionStorage?: boolean
+) => {
+  const storage = getStorage(useSessionStorage);
+  const time = Math.floor(new Date().getTime() / 1000);
+  storage.setItem(storageKey, JSON.stringify(data));
+  storage.setItem(getDateKey(storageKey), time.toString());
 };
 
 export const retrieve = (
-	storageKey: string,
-	minutesToExpiration: number,
-	useSessionStorage?: boolean
+  storageKey: string,
+  minutesToExpiration: number,
+  useSessionStorage?: boolean
 ) => {
-	const storage = getStorage(useSessionStorage);
-	const storageDate = +storage.getItem(getDateKey(storageKey));
-	let data = storage.getItem(storageKey);
-	if (!!data && !!storageDate) {
-		try {
-			const nowMs = +(new Date());
-			const storageMs = +(new Date(storageDate)) * 1000;
-			const minutesOld = Math.ceil(
-				nowMs - storageMs / 1000 / 60
-			);
-			if (minutesOld > minutesToExpiration) {
-				clear(storageKey, useSessionStorage);
-				data = null;
-			} else {
-				data = JSON.parse(data);
-			}
-		} catch (e) {}
-	}
-	return data;
-};
-
-export const clear = (
-	storageKey: string,
-	useSessionStorage?: boolean
-) => {
-	const storage = getStorage(useSessionStorage);
-	delete storage[storageKey];
-	delete storage[getDateKey(storageKey)];
+  const storage = getStorage(useSessionStorage);
+  const storageDate = +storage.getItem(getDateKey(storageKey));
+  let data = !!storageDate
+    ? storage.getItem(storageKey)
+    : null;
+  if (!!data) {
+    const nowMs = +(new Date());
+    const storageMs = +(new Date(storageDate)) * 1000;
+    const minutesOld = Math.ceil(
+      nowMs - storageMs / 1000 / 60
+    );
+    if (minutesOld > minutesToExpiration) {
+      clear(storageKey, useSessionStorage);
+      data = null;
+    } else {
+      try {
+        data = JSON.parse(data);
+      } catch (e) {}
+    }
+  }
+  return data;
 };
